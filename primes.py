b14bfa621f845347092e95e481354cc2fe5db576
Benjamin Groom <benjamin.groom@kcl.ac.uk>
2022-09-27 13:42:46 +0100

    Add solution code.

--- primes.py
+++ primes.py
@@ -2,5 +2,13 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+	list = []
+	num = 2
+	while len(list) < number_of_primes:
+		for divisor in range(2, num + 1):
+			if divisor == num:
+				list.append(num)
+			elif num % divisor == 0:
+				break
+		num += 1
+	return list
